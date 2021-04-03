2ed8de42683b5902b8bdd8045f6a583e1c2e042d
Tyler Nijmeh <tylernij@gmail.com>
2021-04-02 19:16:17 -0700

    Create the entire app
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

---
+++ app/src/main/java/com/draco/mfstrim/fragments/MainPreferenceFragment.kt
@@ -0,0 +1,88 @@
+package com.draco.mfstrim.fragments
+
+import android.content.*
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.text.format.DateUtils
+import androidx.preference.*
+import com.draco.mfstrim.R
+import com.draco.mfstrim.repositories.constants.SettingsConstants
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import com.google.android.material.snackbar.Snackbar
+
+class MainPreferenceFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+    private lateinit var interval: ListPreference
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.main, rootKey)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        interval = findPreference(getString(R.string.pref_interval_key))!!
+
+        refreshSettings()
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        when (preference.key) {
+            getString(R.string.pref_developer_key) -> openURL(getString(R.string.developer_url))
+            getString(R.string.pref_source_key) -> openURL(getString(R.string.source_url))
+            getString(R.string.pref_contact_key) -> openURL(getString(R.string.contact_url))
+            getString(R.string.pref_licenses_key) -> {
+                val intent = Intent(requireContext(), OssLicensesMenuActivity::class.java)
+                startActivity(intent)
+            }
+            else -> return super.onPreferenceTreeClick(preference)
+        }
+        return true
+    }
+
+    /**
+     * Update the UI to show the new constants
+     */
+    private fun refreshSettings() {
+        val ms = Settings.Global.getLong(requireContext().contentResolver, SettingsConstants.FSTRIM_MANDATORY_INTERVAL, 3 * DateUtils.DAY_IN_MILLIS)
+        interval.value = ms.toString()
+    }
+
+    /**
+     * Take the UI settings and apply them as constants
+     */
+    private fun applySettings() {
+        val ms = interval.value.toLong()
+        Settings.Global.putLong(requireContext().contentResolver, SettingsConstants.FSTRIM_MANDATORY_INTERVAL, ms)
+    }
+
+    /**
+     * Open a URL for the user
+     */
+    private fun openURL(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Snackbar.make(requireView(), getString(R.string.snackbar_intent_failed), Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * When settings are changed, apply the new config
+     */
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        applySettings()
+    }
+}
---
+++ app/src/main/java/com/draco/mfstrim/repositories/constants/SettingsConstants.kt
@@ -0,0 +1,5 @@
+package com.draco.mfstrim.repositories.constants
+
+object SettingsConstants {
+    const val FSTRIM_MANDATORY_INTERVAL =   "fstrim_mandatory_interval"
+}
